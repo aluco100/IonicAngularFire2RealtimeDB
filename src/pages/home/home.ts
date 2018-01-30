e2a569c4bc118b8aefd31116e6e983eb83422653
Alfredo Luco <aluco100@gmail.com>
2018-01-30 16:18:53 -0300

    login de usuarios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
@@ -10,13 +12,27 @@ export class HomePage {
 
   public collection: any;
   public messages: Array<any>;
+  public user:any;
 
   @Input() public newMessage:string;
 
-  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+  constructor(public navCtrl: NavController,
+    private db: AngularFireDatabase,
+    private auth: AngularFireAuth) {
     this.db.list("chat").valueChanges().subscribe(data => {
       console.log(data);
       this.messages = data;
+    });
+
+    this.auth.authState.subscribe(user => {
+      console.log(user);
+      if(user){
+        this.user = user;
+        console.log("user signed in: "+user.uid);
+      }else{
+        this.auth.auth.signInAnonymously()
+                      .catch(error => console.log("Error in log in: "+error));
+      }
     });
     // this.db.list("chat").push({
     //   message : "message1"
@@ -25,7 +41,8 @@ export class HomePage {
 
   addMessage(){
     this.db.list("chat").push({
-      message : this.newMessage
+      message : this.newMessage,
+      uid: this.user.uid
     });
     this.newMessage = "";
   }
